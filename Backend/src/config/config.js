0650b49f4fa54c45d8d3d00931034e4d74c5f0ef
Aditya Sharma <adity202212@gmail.com>
2025-04-26 11:23:03 +0530

    Update config.js

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const _config = {
     MONGO_URI:process.env.MONGO_URI,
     JWT_SECRET:process.env.JWT_SECRET,
-    PORT:process.env.PORT
+    PORT:process.env.PORT,
+    CORS_URL:process.env.CORS_URL
 }
  const config = Object.freeze(_config);
 module.exports = config;
