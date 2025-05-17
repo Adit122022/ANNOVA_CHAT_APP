1557d7b6a4d86543184fe2b742b75e7257e807cb
Aditya Sharma <adity202212@gmail.com>
2025-05-17 18:10:13 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const  axiosInstance = axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL:"https://annova-chat-app.onrender.com/api",
     withCredentials:true,});
 
