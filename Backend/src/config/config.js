ec46d6243fe849b2ecba3ff506341e336ca1222a
Adit122022 <adity202212@gmail.com>
2025-05-17 17:27:31 +0530

    Update package.json scripts and modify CORS settings in app.js

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const _config = {
     MONGO_URI:process.env.MONGO_URI,
     JWT_SECRET:process.env.JWT_SECRET,
-    PORT:process.env.PORT,
-    CORS_URL:process.env.CORS_URL
+    PORT:process.env.PORT
 }
  const config = Object.freeze(_config);
 module.exports = config;
