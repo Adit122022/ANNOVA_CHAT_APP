3be4e13baa282a8087c5a48d3614a72fd43815e2
Aditya Sharma <adity202212@gmail.com>
2025-07-10 17:01:58 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,10 @@ import {app ,server ,io} from './lib/Socket.js'
 import cookieParser from 'cookie-parser';
 //  middelwares bhar vale
 app.use(express.json())
-app.use(cors({origin :"https://annova-chat-app.vercel.app" , credentials:true}))
+app.use(cors({ origin: "https://annova-chat-app.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],}))
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
