5df98c946ee8e11bae4bd59862fa31466f881081
Aditya Sharma <adity202212@gmail.com>
2025-07-10 17:13:42 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,29 +1,20 @@
 import express from 'express';
-import cors from 'cors';
+import cors from 'cors'
+import {app ,server ,io} from './lib/Socket.js'
 import cookieParser from 'cookie-parser';
+//  middelwares bhar vale
+app.use(express.json())
+app.use(cors({origin :"https://annova-chat-app.vercel.app" , credentials:true}))
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
-import { app, server, io } from './lib/Socket.js';
+import authRoutes from './routes/auth.route.js'
+import messagesRoute from './routes/messages.route.js'
 
-const corsOptions = {
-  origin: "https://annova-chat-app.vercel.app",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
 
-// Handle CORS
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // <-- 💥 This handles preflight requests properly
+app.use('/api/auth' , authRoutes)
+app.use('/api/messages' , messagesRoute)
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
-// Routes
-import authRoutes from './routes/auth.route.js';
-import messagesRoute from './routes/messages.route.js';
-
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messagesRoute);
 
 export default server;
